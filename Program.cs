f61bb9fd62c6dfda1b67e13dbfdeb4c13130fc21
DaniilAnd <lisichkindan@yandex.ru>
2019-10-24 22:57:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BubleSort
+{
+    class Program
+    {
+        public static int[] BubleSort( int [] vs)
+        {
+            for (int i = 0; i < vs.Length -1 ; i++)
+            {
+                for (int j = 0; j < vs.Length - i - 1; j++)
+                {
+                    if (vs[j] > vs[j +1])
+                    {
+                         int temp = vs[j];
+                        vs[j] = vs[j + 1];
+                        vs[j + 1] = temp;
+                    }
+
+                }
+            }
+            return vs;
+        }
+        static void Main(string[] args)
+        {
+            int[] vs = new int[]  {1,5,7,3,2 };
+            
+            BubleSort(vs);
+            
+            for (int i = 0; i < vs.Length; i++)
+            {
+                Console.WriteLine(vs[i]);
+            }
+        }
+    }
+}
